6a3665b30a2a14cae80e7482485cf6fd27e69f62
Lakshay Bhala <lakshaybhala@gmail.com>
2019-06-25 21:43:12 +0100

    Fix error with packages
    Signed-off-by: Lakshay Bhala <lakshaybhala@gmail.com>

--- home/.emacs.d/custom.el
+++ home/.emacs.d/custom.el
@@ -5,12 +5,10 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" default)))
+    ("0daf22a3438a9c0998c777a771f23435c12a1d8844969a28f75820dd71ff64e1" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" default)))
  '(package-selected-packages
    (quote
-    (word-count-mode pdf-view-mode ebib vimrc-mode sass-mode color-theme-sanityinc-tomorrow
-        scss-mode auctex multiple-cursors dumb-jump yaml-mode rust-mode markdown-mode fish-mode
-        company-sourcekit))))
+    (word-count-mode pdf-view-mode ebib vimrc-mode sass-mode color-theme-sanityinc-tomorrow scss-mode auctex multiple-cursors dumb-jump yaml-mode rust-mode markdown-mode fish-mode company-sourcekit))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
